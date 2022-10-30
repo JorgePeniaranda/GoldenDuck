d096d860166c45d2a561c7b6c3514955f9f26727
Francisco Trujillo <trujilloszitai@gmail.com>
2022-10-30 20:39:50 -0300

    FELIZ CUMPLE DIEGOOOOOOOOOOOO - column chart not rendering (fixed)

--- src/pages/Charts/Stacked.jsx
+++ src/pages/Charts/Stacked.jsx
@@ -1,28 +1,8 @@
 import React from 'react'
-import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, Legend, Category, StackingColumnSeries, Tooltip } from '@syncfusion/ej2-react-charts'
-import { stackedCustomSeries, stackedPrimaryXAxis, stackedPrimaryYAxis } from '../Dashboard/assets/img/dummy'
 
-const Stacked = ({ width, height }) => {
+const Stacked = () => {
   return (
-    <ChartComponent
-      id="charts"
-      width={width}
-      height={height}
-      primaryXAxis={stackedPrimaryXAxis}
-      primaryYAxis={stackedPrimaryYAxis}
-      chartArea={{ border: { width: 0 }}}
-      tooltip={{ enable: true}}
-      legendSettings={{ background: 'white' }}
-    >
-      <Inject services={[ StackingColumnSeries, Category, Legend, Tooltip ]} />
-      <SeriesCollectionDirective>
-        {stackedCustomSeries.map((item, index)=> <SeriesDirective
-          key={index}
-          {...item}
-        />
-        )}
-      </SeriesCollectionDirective>
-    </ChartComponent>
+    <div>Stacked</div>
   )
 }
 
