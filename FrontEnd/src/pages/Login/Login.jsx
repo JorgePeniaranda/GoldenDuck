85a0d167d63bcfa911cd6a6ae5f6e11c4abe2c47
Francisco Trujillo <trujilloszitai@gmail.com>
2022-11-18 17:44:25 -0300

    BEST INPUTS EVER

--- FrontEnd/src/pages/Login/Login.jsx
+++ FrontEnd/src/pages/Login/Login.jsx
@@ -1,18 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
 import Input from '@mui/material/Input';
 import InputLabel from '@mui/material/InputLabel';
 import InputAdornment from '@mui/material/InputAdornment';
 import FormControl from '@mui/material/FormControl';
 import TextField from '@mui/material/TextField';
-import AccountCircle from '@mui/icons-material/AccountCircle';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+
 
 import "./login.scss";
 
 import NavList from "../../components/Navbar/Nav";
 
 const Login = () => {
+
+  const [values, setValues] = useState({
+    password: '',
+    showPassword: false,
+  });
+
+  const handleClickShowPassword = () => {
+    setValues({
+      ...values,
+      showPassword: !values.showPassword,
+    });
+  };
+
+  const handleMouseDownPassword = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <main id="loginSection">
       <NavList.SingleNavResponsive />
@@ -28,12 +48,28 @@ const Login = () => {
         <div id="loginForm">
           <div className="loginInputs">
             <h3>Iniciar Sesión</h3>
-            <input type="text" placeholder="Usuario" />
-{/*               <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
-                <AccountCircle />
-                <TextField id="input-with-sx" label="Usuario..." variant="standard" />
-              </Box> */}
-            <input type="password" placeholder="Contraseña" />
+            <Box sx={{ display: 'flex', alignItems: 'flex-end', width: '50%' }}>
+              <TextField id="input-with-sx" label="Usuario" variant="standard" />
+            </Box>
+
+            <FormControl sx={{ m: 1, width: '50%' }} variant="standard">
+              <InputLabel htmlFor="standard-adornment-password">Contraseña</InputLabel>
+              <Input
+                id="standard-adornment-password"
+                type={values.showPassword ? 'text' : 'password'}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                    >
+                      {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+              />
+            </FormControl>
             <Link to="/forgot-my-password">Olvide mi Contraseña</Link>
             <Link to="/signin" id="registeMobileButton">Crear Cuenta</Link>
             <Link to="/dashboard/money" id="login">
