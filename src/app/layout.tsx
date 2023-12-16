e896e47b2cf7d6870fe98ccf1361b28274fa2540
JorgePeniaranda <peniarandajorge@gmail.com>
2023-12-16 12:54:04 -0300

    chore: initialization next.js project

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { Baloo_2 } from 'next/font/google'
+import './globals.css'
+
+const inter = Baloo_2({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'GoldenDuck',
+  description: 'GoldenDuck',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="es">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
