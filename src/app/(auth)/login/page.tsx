ac45934d2682b4847f1dbb8cc162f408d421253c
JorgePeniaranda <peniarandajorge@gmail.com>
2024-01-06 22:38:31 -0300

    feat: replicate login page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,8 +1,30 @@
 import Navbar from '@/components/organisms/navbar/base'
 import style from './styles.module.scss'
+import Text from '@/components/atoms/text/Text'
+import InternalLinkText from '@/components/atoms/text/InternalLinkText'
 
 export default function Login() {
   return (
-    <Navbar/>
+    <>
+      <Navbar/>
+      <main className={style.LoginSection}>
+        <section>
+          <article>
+            <Text tag='h1' size={"2.6rem"} weight='800'>Crear tu cuenta Golden Duck ahora mismo</Text>
+            <Text>¡Registrate para obtener los beneficios que Golden Duck te ofrece!</Text>
+          </article>
+          <InternalLinkText href='/signin'>Registrarse</InternalLinkText>
+        </section>
+        <section>
+          <form>
+            <Text tag='h1' size={"1.9rem"} weight='700'>Iniciar Sesión</Text>
+            <input type="text" placeholder='Usuario'/>
+            <input type="password" placeholder='Contraseña'/>
+            <InternalLinkText href='/forgot'>Olvide mi contraseña</InternalLinkText>
+            <button>Ingresar</button>
+          </form>
+        </section>
+      </main>
+    </>
   )
 }
