d3b8e2dfc18194ffdb8795b8a9d58f3afac64f83
JorgePeniaranda <peniarandajorge@gmail.com>
2024-01-06 22:36:32 -0300

    feat: create not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,21 +6,20 @@ import InternalLinkText from "@/components/atoms/text/InternalLinkText";
 
 export default function NotFound() {
   return (
-    <main>
+    <>
         <Navbar/>
-        <section>
-            <article className="w-full h-[calc(83vh-10rem)] grid place-items-center gap-10 select-none">
-                <figure className="transition hover:scale-110">
-                    <InternalLinkText href='/' arialLabel='Go Home'>
-                        <Image src="/assets/img/designs/404.webp" width={500} height={600} alt="404 Página no encontrada"/>
-                    </InternalLinkText>
-                </figure>
-                <Text size={"2rem"} weight="700">Página no encontrada</Text>
-            </article>
-        </section>
-        <footer>
-            <Footer/>
-        </footer>
-    </main>
+        <main>
+            <section>
+                <article className="w-full h-[calc(100vh-var(--base-nav-height))] grid place-items-center gap-10 select-none">
+                    <figure className="transition hover:scale-110">
+                        <InternalLinkText href='/' arialLabel='Go Home'>
+                            <Image className="h-3/4" src="/assets/img/designs/404.webp" width={500} height={600} alt="404 Página no encontrada"/>
+                        </InternalLinkText>
+                    </figure>
+                    <Text size={"2rem"} weight="700">Página no encontrada</Text>
+                </article>
+            </section>
+        </main>
+    </>
   )
 }
