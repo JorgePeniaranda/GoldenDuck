6a4cd0196aa603f07b8312789c5bfad934607b8d
JorgePeniaranda <peniarandajorge@gmail.com>
2024-01-09 15:07:53 -0300

    feat: implement internal and external links with button style

--- src/components/molecules/buttons/BaseButton/index.tsx
+++ src/components/molecules/buttons/BaseButton/index.tsx
@@ -1,30 +1,50 @@
 import { ReactNode } from "react";
 import style from "./styles.module.scss"
+import ExternalLinkText from "@/components/atoms/text/ExternalLinkText";
+import InternalLinkText from "@/components/atoms/text/InternalLinkText";
 
 interface Props{
     children: ReactNode
     className?: string
-    tag?: 'button' | 'InternalLinkText' | 'ExternalLinkText'
+    type?: 'button' | 'InternalLinkText' | 'ExternalLinkText'
     yPadding?: string;
     xPadding?: string;
     backgroundColor?: string;
     fontWeight?: "100" | "200" | "300" | "400" | "500" | "600" | "700" | "800" | "900";
     fontSize?: string;
+    fontColor?: string;
+    href?: string;
 }
 
-export default function BaseButton({children, className, tag = 'button', yPadding = "0.5rem", xPadding = "1rem", backgroundColor = "var(--primary)", fontWeight, fontSize}: Props) {
-  const Tag = tag as keyof JSX.IntrinsicElements;
-
-  return (
-    <Tag className={`${style.BaseButton} ${className}`} style={
-      {
-        padding: `${yPadding} ${xPadding}`,
-        backgroundColor: backgroundColor,
-        fontWeight: fontWeight,
-        fontSize: fontSize,
-      }
-    }>
-      {children}
-    </Tag>
-  )
+export default function BaseButton({children, className, type = 'button', yPadding = "0.5rem", xPadding = "1rem", backgroundColor = "var(--primary)", fontWeight, fontSize, fontColor, href}: Props) {
+  const props = {
+    className: `${style.BaseButton} ${className}`,
+    style: {
+      padding: `${yPadding} ${xPadding}`,
+      backgroundColor: backgroundColor,
+      fontWeight: fontWeight,
+      fontSize: fontSize,
+      color: fontColor
+    }
+  }
+  switch (type) {
+    case 'button':
+      return (
+        <button {...props}>
+          {children}
+        </button>
+      )
+    case 'InternalLinkText':
+      return (
+        <InternalLinkText href={href || "#"} {...props}>
+          {children}
+        </InternalLinkText>
+      )
+    case 'ExternalLinkText':
+      return (
+        <ExternalLinkText href={href || "#"} {...props}>
+          {children}
+        </ExternalLinkText>
+      )
+  }
 }
