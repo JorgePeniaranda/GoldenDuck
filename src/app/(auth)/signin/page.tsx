93d9c78a50e65d0ecb7ee066c34ffb913edcb895
JorgePeniaranda <peniarandajorge@gmail.com>
2024-01-10 10:44:30 -0300

    fix options text position

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -3,6 +3,7 @@
 import { FormEvent, useState } from 'react'
 import { redirect } from 'next/navigation'
 import GetUserInfo from './get-user-info'
+import style from './styles.module.scss'
 import ConfirmUserInfo from './confirm-user-info'
 import ContainerCenteredItemsWithNavbar from '@/components/pages/container-centered-items-with-navbar'
 import Text from '@/components/atoms/text/Text'
@@ -32,23 +33,24 @@ export default function Login() {
   }
 
   return (
-    <ContainerCenteredItemsWithNavbar>
+    <ContainerCenteredItemsWithNavbar className={style.SignIn}>
       <Text tag="h1" size={'2.6rem'} weight="700">
         Registrarse
       </Text>
       {steps[step](formActions)} {/* tarea para casa */}
-      {step === 0 ? (
-        <InternalLinkText
-          href="/login"
-          className="absolute bottom-10 text-[var(--LinkColor)] underline"
-        >
-          Ya tengo una cuenta
-        </InternalLinkText>
-      ) : (
-        <p onClick={formActions.back} className="absolute bottom-10 text-[var(--LinkColor)] underline cursor-pointer">
-          Volver
-        </p>
-      )}
+      <div className={style.OptionContainer}>
+        {step === 0 ? (
+          <InternalLinkText
+            href="/login"
+          >
+            Ya tengo una cuenta
+          </InternalLinkText>
+        ) : (
+          <p onClick={formActions.back}>
+            Volver
+          </p>
+        )}
+      </div>
     </ContainerCenteredItemsWithNavbar>
   )
 }
