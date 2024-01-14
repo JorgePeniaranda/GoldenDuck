bc8db02c8d481e186d45b433ca043f67381f217f
JorgePeniaranda <peniarandajorge@gmail.com>
2024-01-13 22:41:21 -0300

    Add manifest.json and update metadata in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,8 +8,24 @@ const Poppins = PoppinsFont({
 })
 
 export const metadata: Metadata = {
-  title: 'GoldenDuck',
-  description: 'GoldenDuck',
+  title: 'Golden Duck',
+  description: 'Una banca online donde podrá, no solo gestionar su dinero, sino que incluso invertirlo. Siempre llevando un registro de cuanto dinero es ingresado y cuanto dinero es gastado, contando con categorías para saber en qué lo gasta. También podrá pagar servicios, tales como servicios de Telefonía Móvil o servicios esenciales.',
+  authors: {
+    name: "Lycokat",
+    url: "https://lycokat.netlify.app/",
+  },
+  category: "Virtual Wallet",
+  colorScheme: "light dark",
+  creator: "Lycokat",
+  generator: "Next.js",
+  manifest: "/manifest.json",
+  publisher: "Vercel",
+  robots: { index: true, follow: true },
+  themeColor: [
+    { media: "(prefers-color-scheme: dark)", color: "#1f1f1f" },
+    { media: "(prefers-color-scheme: light)", color: "#f9f7f7" },
+  ],
+  viewport: { width: "device-width", initialScale: 1 },
 }
 
 export default function RootLayout({
