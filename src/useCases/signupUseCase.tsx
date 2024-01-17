47b22572a42f926893c5800295af89ca7faeb334
JorgePeniaranda <peniarandajorge@gmail.com>
2024-01-17 19:32:38 -0300

    refactor: add required_error message in signupSchema

--- src/useCases/signupUseCase.tsx
+++ src/useCases/signupUseCase.tsx
@@ -4,27 +4,31 @@ import { SignupForm } from '@/types'
 import { z } from 'zod'
 
 export const SignUpSchema = z.object({
-  name: z.string().min(1, { message: 'El nombre es requerido' }),
+  name: z
+    .string({ required_error: 'El nombre es requerido' })
+    .min(1, { message: 'El nombre es requerido' }),
   lastName: z.string().min(1, { message: 'El apellido es requerido' }),
   dni: z.coerce
-    .number()
+    .number({ required_error: 'El dni es requerido' })
     .min(1, { message: 'El dni es requerido' })
     .min(10000000, { message: 'El dni debe ser valido' })
     .max(99999999, { message: 'El dni debe ser valido' }),
   email: z
-    .string()
+    .string({ required_error: 'El email es requerido' })
     .email({ message: 'El email debe ser valido' })
     .min(1, { message: 'El email es requerido' }),
   phoneNumber: z.coerce
-    .number()
+    .number({ required_error: 'El número telefónico es requerido' })
     .min(1, { message: 'El número telefónico es requerido' })
     .min(1000000000, { message: 'El número telefónico debe ser valido' })
     .max(9999999999, { message: 'El número telefónico debe ser valido' }),
   password: z
-    .string()
+    .string({ required_error: 'La contraseña es requerida' })
     .min(8, { message: 'La contraseña debe tener al menos 8 caracteres' })
     .min(1, { message: 'La contraseña es requerida' }),
-  address: z.string().min(1, { message: 'La dirección es requerida' }),
+  address: z
+    .string({ required_error: 'La dirección es requerida' })
+    .min(1, { message: 'La dirección es requerida' }),
   birthDate: z.coerce.date({ required_error: 'El email es requerido' }),
   sex: z.enum(['male', 'female'], { required_error: 'El sexo es requerido' }),
 })
