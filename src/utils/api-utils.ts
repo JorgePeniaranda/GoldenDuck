ca27abb5253c07a13f909a3301381cb1864aa2bf
jorgepeniaranda <peniarandajorge@gmail.com>
2024-01-19 19:41:27 -0300

    refactor: rename checktoken + feat: implement check-unauthorized tokens

--- src/utils/api-utils.ts
+++ src/utils/api-utils.ts
@@ -2,7 +2,7 @@ import JWT from '@/services/jwtService'
 
 const jwt = new JWT()
 
-export const CheckToken = async (req: Request) => {
+export const CheckAuthorizedToken = async (req: Request) => {
   const token = await req.headers.get('token')
 
   if (!token) {
@@ -14,7 +14,7 @@ export const CheckToken = async (req: Request) => {
 
   const decodedToken = jwt.verifyToken(token)
 
-  if (!decodedToken) {
+  if (!decodedToken || typeof decodedToken === 'string') {
     return {
       error: 'El token no es valido o ha expirado',
       status: 401,
@@ -24,5 +24,44 @@ export const CheckToken = async (req: Request) => {
   return {
     success: true,
     userId: decodedToken.userId,
+  }
+}
+
+export async function CheckUnAuthorizedToken(req: Request) {
+  const { code, token } = await req.json()
+
+  if (!code) {
+    return {
+      error: 'No se ha enviado el código.',
+      status: 400,
+    }
+  }
+
+  if (!token) {
+    return {
+      error: 'No se ha enviado el token.',
+      status: 400,
+    }
+  }
+
+  const decodedToken = await jwt.verifyToken(token)
+
+  if (!decodedToken || typeof decodedToken === 'string') {
+    return {
+      error: 'El token no es valido o ha expirado',
+      status: 401,
+    }
+  }
+
+  if (decodedToken.code !== code) {
+    return {
+      error: 'Código incorrecto',
+      status: 401,
+    }
+  }
+
+  return {
+    success: true,
+    email: decodedToken.email,
   }
 }
