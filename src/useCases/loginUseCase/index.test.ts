3bdc2ed058c663357cbba2d2058dc8b6d71e3ac3
jorgepeniaranda <peniarandajorge@gmail.com>
2024-01-29 13:46:45 -0300

    refactor: remove unnecessary async

--- src/useCases/loginUseCase/index.test.ts
+++ src/useCases/loginUseCase/index.test.ts
@@ -1,13 +1,15 @@
 import { LoginForm } from '@/types'
 import { CheckForm } from '.'
+import axios from 'axios'
 
 const validForm: LoginForm = { email: 'test@gmail.com', password: '12345678' }
+jest.mock('axios')
 
 describe('Validate Login Form', () => {
-  it('must return true if the email is valid', async () => {
+  it('must return true if the email is valid', () => {
     expect(CheckForm(validForm)).toBeTruthy()
   })
-  it('must return false if the email is invalid', async () => {
+  it('must return false if the email is invalid', () => {
     expect(CheckForm({ ...validForm, email: 'test' })).toBeFalsy()
   })
 })
--- src/useCases/registerUseCase/index.test.ts
+++ src/useCases/registerUseCase/index.test.ts
@@ -14,37 +14,37 @@ const validForm: SignupForm = {
 }
 
 describe('Validate Register Form', () => {
-  it('must return true if the form format is valid', async () => {
+  it('must return true if the form format is valid', () => {
     expect(CheckForm(validForm)).toBeTruthy()
   })
-  it('must return false if the name has invalid characters', async () => {
+  it('must return false if the name has invalid characters', () => {
     expect(CheckForm({ ...validForm, name: '_t3st_' })).toBeFalsy()
   })
-  it('must return false if the lastName has invalid characters', async () => {
+  it('must return false if the lastName has invalid characters', () => {
     expect(CheckForm({ ...validForm, lastName: '_t3st_' })).toBeFalsy()
   })
-  it('must return false if the phoneNumber has invalid characters', async () => {
+  it('must return false if the phoneNumber has invalid characters', () => {
     expect(CheckForm({ ...validForm, phoneNumber: '123456789a' })).toBeFalsy()
   })
-  it('must return false if the phoneNumber has invalid long', async () => {
+  it('must return false if the phoneNumber has invalid long', () => {
     expect(CheckForm({ ...validForm, phoneNumber: '123456789' })).toBeFalsy()
   })
-  it('must return false if the dni has invalid characters', async () => {
+  it('must return false if the dni has invalid characters', () => {
     expect(CheckForm({ ...validForm, dni: '1234567a' })).toBeFalsy()
   })
-  it('must return false if the dni has invalid long', async () => {
+  it('must return false if the dni has invalid long', () => {
     expect(CheckForm({ ...validForm, dni: '1234567' })).toBeFalsy()
   })
-  it('must return false if the birthDate has invalid format', async () => {
+  it('must return false if the birthDate has invalid format', () => {
     expect(CheckForm({ ...validForm, birthDate: '01/01/2000a' })).toBeFalsy()
   })
-  it('must return false if the address has invalid characters', async () => {
+  it('must return false if the address has invalid characters', () => {
     expect(CheckForm({ ...validForm, address: '_test_' })).toBeFalsy()
   })
-  it('must return false if the email is invalid', async () => {
+  it('must return false if the email is invalid', () => {
     expect(CheckForm({ ...validForm, email: 'test' })).toBeFalsy()
   })
-  it('must return false if the password has invalid long', async () => {
+  it('must return false if the password has invalid long', () => {
     expect(CheckForm({ ...validForm, password: 'test' })).toBeFalsy()
   })
 })
