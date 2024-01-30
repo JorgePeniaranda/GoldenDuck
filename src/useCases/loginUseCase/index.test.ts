fbbb7062a2b70de351e78358ba204e9af449b1d7
jorgepeniaranda <peniarandajorge@gmail.com>
2024-01-29 21:30:47 -0300

    feat: Add login function and test

--- src/useCases/loginUseCase/index.test.ts
+++ src/useCases/loginUseCase/index.test.ts
@@ -1,5 +1,6 @@
 import { LoginForm } from '@/types'
-import { CheckForm } from '.'
+import { CheckForm, login } from '.'
+import axios from 'axios'
 
 const validForm: LoginForm = { email: 'test@gmail.com', password: '12345678' }
 
@@ -9,5 +10,22 @@ describe('Validate Login Form', () => {
   })
   it('must return false if the email is invalid', () => {
     expect(CheckForm({ ...validForm, email: 'test' })).toBeFalsy()
+  })
+})
+
+describe('login', () => {
+  it('must be a function', () => {
+    expect(typeof login).toBe('function')
+  })
+  it('UpdatePassword should return a status code', async () => {
+    const mockResponse = {
+      status: 200,
+      data: {
+        message: 'test',
+      },
+    }
+    axios.post = jest.fn().mockResolvedValue(mockResponse)
+    const response = await login(validForm)
+    expect(response).toEqual(mockResponse.status)
   })
 })
