adc578b909460139e1ad923e6edb37c168a84af0
jorgepeniaranda <peniarandajorge@gmail.com>
2024-02-04 19:16:49 -0300

    feat: implement react-hook-form, display errors and create handlersubmits

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -2,48 +2,200 @@
 
 import { useState } from 'react'
 import { redirect } from 'next/navigation'
-import GetUserInfo from './get-user-info'
 import style from './styles.module.scss'
-import ConfirmUserInfo from './confirm-user-info'
 import InternalLinkText from '@/components/atoms/text/InternalLinkText'
 import { SignupForm } from '@/types'
-import { CreateUser } from '@/useCases/registerUseCase'
+import {
+  CreateUser,
+  SignUpSchema,
+  checkConfirmationCode,
+  generateConfirmationCode,
+} from '@/useCases/registerUseCase'
+import Text from '@/components/atoms/text/Text'
+import BaseButton from '@/components/molecules/buttons/base-button'
+import ErrorSpan from '@/components/atoms/text/ErrorSpan'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import ReactCodeInput from 'react-code-input'
+import Alerts from '@/services/alertService'
+import { ErrorsHandler, ValidationError } from '@/services/errorService'
 
 export default function Signin() {
   const [step, setStep] = useState<number>(0)
-  const [form, setForm] = useState<SignupForm>({
-    name: '',
-    lastName: '',
-    phoneNumber: '',
-    dni: '',
-    birthDate: '',
-    address: '',
-    email: '',
-    password: '',
-    sex: '',
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors, isSubmitting },
+  } = useForm<SignupForm>({
+    resolver: zodResolver(SignUpSchema),
+  })
+  const [code, setCode] = useState<string>('')
+
+  const onSubmitGetData = handleSubmit(async () => {
+    try {
+      await generateConfirmationCode(
+        watch('email'),
+        watch('dni'),
+        watch('phoneNumber'),
+      ).catch((err) => {
+        throw new ValidationError(err.response.data.error)
+      })
+
+      setStep(step + 1)
+    } catch (e) {
+      const { error } = ErrorsHandler(e)
+      return Alerts.error(error)
+    }
   })
 
-  const FormActions = {
-    next: () => {
-      setStep(step + 1)
-    },
-    back: () => {
-      setStep(step - 1)
-    },
-    submit: () => {
-      CreateUser(form)
-    },
+  const onSubmit = handleSubmit(async (form) => {
+    try {
+      await checkConfirmationCode(watch('email'), code).catch((err) => {
+        throw new ValidationError(err.response.data.error)
+      })
+
+      await CreateUser(form).catch((err) => {
+        throw new ValidationError(err.response.data.error)
+      })
+        
+      Alerts.success('Usuario creado con exito', () => {
+        location.href = '/dashboard'
+      })
+    } catch (e) {
+      const { error } = ErrorsHandler(e)
+      return Alerts.error(error)
+    }
+  })
+
+  const handleBack = () => {
+    setStep(step - 1)
   }
 
   switch (step) {
     case 0:
       return (
         <>
-          <GetUserInfo
-            FormActions={FormActions}
-            form={form}
-            setForm={setForm}
-          />
+          <form onSubmit={onSubmitGetData} className={style.GetInfo}>
+            <section>
+              <Text tag="h2">Datos</Text>
+              <article>
+                <label>
+                  <input
+                    type="text"
+                    placeholder="nombre"
+                    {...register('name')}
+                  />
+                  <ErrorSpan show={!!errors.name} align="center">
+                    {errors.name?.message}
+                  </ErrorSpan>
+                </label>
+                <label>
+                  <input
+                    type="text"
+                    placeholder="apellido"
+                    {...register('lastName')}
+                  />
+                  <ErrorSpan show={!!errors.lastName} align="center">
+                    {errors.lastName?.message}
+                  </ErrorSpan>
+                </label>
+                <label>
+                  <input type="text" placeholder="dni" {...register('dni')} />
+                  <ErrorSpan show={!!errors.dni} align="center">
+                    {errors.dni?.message}
+                  </ErrorSpan>
+                </label>
+              </article>
+            </section>
+            <section>
+              <Text tag="h2">Cuenta y Contacto</Text>
+              <article>
+                <label>
+                  <input
+                    type="email"
+                    placeholder="email"
+                    {...register('email')}
+                  />
+                  <ErrorSpan show={!!errors.email} align="center">
+                    {errors.email?.message}
+                  </ErrorSpan>
+                </label>
+                <label>
+                  <input
+                    type="number"
+                    placeholder="telefono"
+                    {...register('phoneNumber')}
+                  />
+                  <ErrorSpan show={!!errors.phoneNumber} align="center">
+                    {errors.phoneNumber?.message}
+                  </ErrorSpan>
+                </label>
+                <label>
+                  <input
+                    type="password"
+                    placeholder="contraseña"
+                    {...register('password')}
+                  />
+                  <ErrorSpan show={!!errors.password} align="center">
+                    {errors.password?.message}
+                  </ErrorSpan>
+                </label>
+              </article>
+            </section>
+            <section>
+              <Text tag="h2">Información</Text>
+              <article>
+                <label>
+                  <input
+                    type="text"
+                    placeholder="domicilio"
+                    {...register('address')}
+                  />
+                  <ErrorSpan show={!!errors.address} align="center">
+                    {errors.address?.message}
+                  </ErrorSpan>
+                </label>
+                <label>
+                  <input type="date" {...register('birthDate')} />
+                  Fecha de Nacimiento
+                  <ErrorSpan show={!!errors.birthDate} align="center">
+                    {errors.birthDate?.message}
+                  </ErrorSpan>
+                </label>
+                <div id="sex">
+                  <label>
+                    <input type="radio" value="male" {...register('sex')} />
+                    Masculino
+                    <ErrorSpan show={!!errors.sex} align="center">
+                      {errors.sex?.message}
+                    </ErrorSpan>
+                  </label>
+                  <label>
+                    <input type="radio" value="female" {...register('sex')} />
+                    Femenino
+                    <ErrorSpan show={!!errors.sex} align="center">
+                      {errors.sex?.message}
+                    </ErrorSpan>
+                  </label>
+                </div>
+              </article>
+            </section>
+            <Text>
+              Al continuar y enviar este formulario aceptá los{' '}
+              <InternalLinkText href="/terms-and-conditions">
+                Terminos y Condiciones
+              </InternalLinkText>{' '}
+              de Golden Duck
+            </Text>
+            <BaseButton
+              fontSize="1.1rem"
+              fontColor="var(--white)"
+              loading={isSubmitting}
+            >
+              Siguiente
+            </BaseButton>
+          </form>
           <InternalLinkText href="/login" className={style.LinkStyle}>
             Ya tengo una cuenta
           </InternalLinkText>
@@ -51,12 +203,38 @@ export default function Signin() {
       )
     case 1:
       return (
-        <>
-          <ConfirmUserInfo FormActions={FormActions} form={form} />
-          <p onClick={FormActions.back} className={style.LinkStyle}>
+        <section className={style.ConfirmUserInfo}>
+          <article>
+            <Text size={'1.1rem'}>
+              Revisa tu mail{' '}
+              <Text tag="span" weight="700">
+                {watch('email')}
+              </Text>{' '}
+              e ingresa el código recibido, Si no lo encuentras prueba buscarlo
+              en la categoria {'"Spam"'}
+            </Text>
+          </article>
+          <form onSubmit={onSubmit}>
+            <ReactCodeInput
+              type="text"
+              fields={6}
+              inputMode="email"
+              value={code}
+              name="EmailCode"
+              onChange={(e) => setCode(e)}
+            />
+            <BaseButton
+              fontSize="1.1rem"
+              fontColor="var(--white)"
+              loading={isSubmitting}
+            >
+              Confirmar
+            </BaseButton>
+          </form>
+          <p onClick={handleBack} className={style.LinkStyle}>
             Volver
           </p>
-        </>
+        </section>
       )
     default:
       return redirect('/404')
