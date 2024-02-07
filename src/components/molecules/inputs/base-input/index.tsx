d04ffe4982837bbb41e28ed002d7b1376a923557
jorgepeniaranda <peniarandajorge@gmail.com>
2024-02-07 18:47:26 -0300

    refactor: simplify baseInput

--- src/components/molecules/inputs/base-input/index.tsx
+++ src/components/molecules/inputs/base-input/index.tsx
@@ -2,59 +2,23 @@ import { InputProps } from '@/types'
 import styles from './styles.module.scss'
 import classNames from 'classnames'
 
-interface Props extends InputProps {
+interface Props {
+  className?: string
   BaseStyle?: boolean
+  [props: string]: any
 }
 
 export default function BaseInput({
-  BaseStyle = true,
-  type,
-  name,
-  id,
-  onChange,
-  onBlur,
   className,
-  style,
-  required,
-  placeholder,
-  value,
-  disabled,
-  readOnly,
-  autoFocus,
-  autoComplete,
-  pattern,
-  maxLength,
-  minLength,
-  max,
-  min,
-  list,
-  checked,
+  BaseStyle,
+  ...props
 }: Props) {
   const classes = classNames({ [styles.BaseInput]: BaseStyle }, className)
 
   return (
     <input
-      type={type}
-      name={name}
-      id={id}
-      onChange={onChange}
-      onBlur={onBlur}
       className={classes}
-      style={style}
-      required={required}
-      placeholder={placeholder}
-      value={value}
-      disabled={disabled}
-      readOnly={readOnly}
-      autoFocus={autoFocus}
-      autoComplete={autoComplete}
-      pattern={pattern}
-      maxLength={maxLength}
-      minLength={minLength}
-      max={max}
-      min={min}
-      list={list}
-      checked={checked}
+      {...props}
     />
   )
 }
