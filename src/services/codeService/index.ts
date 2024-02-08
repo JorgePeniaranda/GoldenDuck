12e7b6b2d4ee9378a9fb4d4d4e57769ba2a69b39
jorgepeniaranda <peniarandajorge@gmail.com>
2024-02-08 11:49:16 -0300

    feat: implement resend with code email template

--- src/services/codeService/index.ts
+++ src/services/codeService/index.ts
@@ -1,7 +1,11 @@
 import bcrypt from 'bcryptjs'
 import { randomAlphanumeric } from '@/utils'
 import { validations } from '../validationService'
-import { ValidationError } from '../errorService'
+import { EmailError, ErrorsHandler, ValidationError } from '../errorService'
+import { Resend } from 'resend'
+import TemplateCodeEmail from '@/components/templates/email/TemplateCodeEmail'
+
+const resend = new Resend(process.env.RESEND_API_KEY)
 
 export default class ConfirmationCode {
   private code: string
@@ -15,15 +19,35 @@ export default class ConfirmationCode {
   }
 
   sendCode(email: string) {
+    let code = this.code
+
     const checkEmail = validations.email.safeParse(email)
     if (!checkEmail.success)
       throw new ValidationError(checkEmail.error.errors[0].message)
 
-    if (process.env.NODE_ENV === 'development') {
-      return console.log('Código enviado: ' + this.code)
-    }
+    // if (process.env.NODE_ENV === 'development') {
+    //   return console.log('Código enviado: ' + this.code)
+    // }
 
-    return true
+    return (async function () {
+      try {
+        await resend.emails
+          .send({
+            from: 'Acme <onboarding@resend.dev>',
+            to: ['peniarandajorge@gmail.com'],
+            subject: 'Código de verificación | Golden Duck',
+            react: TemplateCodeEmail({
+              code,
+            }),
+          })
+          .catch(() => {
+            throw new EmailError('No se ha podido enviar el mail')
+          })
+      } catch (e) {
+        const { error } = ErrorsHandler(e)
+        return console.error(error)
+      }
+    })()
   }
 
   checkCode(code: string, hash: string) {
