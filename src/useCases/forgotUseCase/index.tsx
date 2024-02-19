2be1ef856f78fe5c3924b19a5c37e8e85f029cfa
JorgePeniaranda <peniarandajorge@gmail.com>
2024-02-19 11:35:58 -0300

    refactor: move forgot handler to use case

--- src/useCases/forgotUseCase/index.tsx
+++ src/useCases/forgotUseCase/index.tsx
@@ -1,3 +1,5 @@
+import Alerts from '@/services/alertService'
+import { ErrorsHandler, ValidationError } from '@/services/errorService'
 import validations from '@/services/validationService'
 import { type ForgotForm } from '@/types'
 import axios, { type AxiosResponse } from 'axios'
@@ -20,3 +22,64 @@ export const checkConfirmationCode = async (email: string, code: string): Promis
 
 export const UpdatePassword = async (ForgotForm: ForgotForm): Promise<AxiosResponse> =>
   await axios.post('/api/forgot', ForgotForm)
+
+
+export const handleNext = (step: number, setStep: (number: number) => void): void => {
+  setStep(step + 1)
+}
+
+export const handleBack = (step: number, setStep: (number: number) => void): void => {
+  setStep(step - 1)
+}
+
+export const onSubmitEmailForm = async (form: { email: string }, step: number, setStep: (number: number) => void) => {
+  try {
+    await generateConfirmationCode(form.email).catch((err) => {
+      throw new ValidationError(err.response.data.error as string)
+    })
+
+    handleNext(step, setStep)
+  } catch (e) {
+    const { error } = ErrorsHandler(e)
+    Alerts.error(error)
+  }
+}
+
+export const onSubmitCodeForm = async (form: { email: string }, code: string, step: number, setStep: (number: number) => void) => {
+  try {
+    await checkConfirmationCode(form.email, code).catch(
+      (err) => {
+        throw new ValidationError(err.response.data.error as string)
+      }
+    )
+
+    handleNext(step, setStep)
+  } catch (e) {
+    const { error } = ErrorsHandler(e)
+    Alerts.error(error)
+  }
+}
+
+export const onSubmitPasswordForm = async (form: ForgotForm, email: string) => {
+  try {
+    if (
+      form.password !== form.confirmPassword
+    ) { Alerts.warning('Las contraseñas no coinciden'); return }
+
+    await UpdatePassword({ ...form, email: email }).catch(
+      (err) => {
+        throw new ValidationError(err.response.data.error as string)
+      }
+    )
+
+    Alerts.success(
+      'Se ha actualizado la contraseña exitosamente',
+      () => {
+        location.href = '/dashboard'
+      }
+    )
+  } catch (e) {
+    const { error } = ErrorsHandler(e)
+    Alerts.error(error)
+  }
+}
