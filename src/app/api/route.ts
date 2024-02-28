a5de30617d06715b41003ebd76a4b736356e0626
jorgepeniaranda <peniarandajorge@gmail.com>
2024-02-28 12:00:42 -0300

    test api

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,34 +1,16 @@
 import { NextResponse } from 'next/server'
 
-const response = NextResponse.json(
-  { date: new Date().toLocaleString() },
-  { status: 418 }
-)
-
-export function GET (): NextResponse {
-  return response
+const response = (): NextResponse => {
+  return NextResponse.json(
+    { date: new Date().toLocaleString() },
+    { status: 418 }
+  )
 }
 
-export function HEAD (): NextResponse {
-  return response
-}
-
-export function POST (): NextResponse {
-  return response
-}
-
-export function PUT (): NextResponse {
-  return response
-}
-
-export function DELETE (): NextResponse {
-  return response
-}
-
-export function PATCH (): NextResponse {
-  return response
-}
-
-export async function OPTIONS (): Promise<NextResponse> {
-  return response
-}
+export const GET = response
+export const HEAD = response
+export const POST = response
+export const PUT = response
+export const DELETE = response
+export const PATCH = response
+export const OPTIONS = response
