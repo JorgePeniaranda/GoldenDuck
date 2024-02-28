c5c05a623bcaf03a091b0106cf79c87168b8afaf
jorgepeniaranda <peniarandajorge@gmail.com>
2024-02-27 23:06:47 -0300

    refactor: update register and forgot endpoints

---
+++ src/app/api/(authorized)/forgot/route.ts
@@ -0,0 +1,61 @@
+import JWT from '@/services/jwtService'
+import { type NextRequest, NextResponse } from 'next/server'
+import { PrismaClient } from '@prisma/client'
+import bcrypt from 'bcryptjs'
+import {
+  GenerateErrorResponse,
+  NotFoundError,
+  ValidationError
+} from '@/services/errorService'
+import validations from '@/services/validationService'
+
+const prisma = new PrismaClient()
+const jwt = new JWT()
+
+export async function POST (req: NextRequest): Promise<NextResponse> {
+  try {
+    // get token and form data
+    const { email: userEmail, password: userPassword } = await req.json()
+
+    // validate email and password
+    const email = await validations.email.parseAsync(userEmail).catch((error) => {
+      throw new ValidationError(error.errors[0].message)
+    })
+    const password = await validations.password.parseAsync(userPassword).catch((error) => {
+      throw new ValidationError(error.errors[0].message)
+    })
+
+    // get user and check if user exists
+    const user = await prisma.users.findFirst({
+      where: { email, deleted: false }
+    })
+    if (user === undefined || user === null) throw new NotFoundError('No existe cuenta creada con ese correo')
+
+    // update password
+    const newUser = await prisma.users.update({
+      where: { id: user.id },
+      data: { password: bcrypt.hashSync(password) }
+    })
+
+    // generate autorized token with id
+    const token = jwt.generateToken({
+      id: newUser.id
+    })
+
+    // generate and send response
+    const response = NextResponse.json(
+      { message: 'Se ha actualizado la contraseña exitosamente' },
+      { status: 201 }
+    )
+    response.cookies.set('token', token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'strict',
+      maxAge: 1000 * 60 * 30,
+      path: '/'
+    })
+    return response
+  } catch (e) {
+    return GenerateErrorResponse(e)
+  }
+}
---
+++ src/app/api/(authorized)/register/route.ts
@@ -0,0 +1,69 @@
+import JWT from '@/services/jwtService'
+import { type NextRequest, NextResponse } from 'next/server'
+import { type SignupForm } from '@/types'
+import { SignUpSchema } from '@/useCases/registerUseCase'
+import { PrismaClient } from '@prisma/client'
+import bcrypt from 'bcryptjs'
+import {
+  ConflictError,
+  GenerateErrorResponse,
+  ValidationError
+} from '@/services/errorService'
+
+const prisma = new PrismaClient()
+const jwt = new JWT()
+
+export async function POST (req: NextRequest): Promise<NextResponse> {
+  try {
+    const userData = await req.json()
+
+    // validate data
+    const data = await SignUpSchema.parseAsync(userData as SignupForm).catch((error) => {
+      throw new ValidationError(error.errors[0].message)
+    })
+
+    // Check if user already exists
+    const checkSameUser = await prisma.users.findFirst({
+      where: {
+        OR: [
+          { dni: data.dni },
+          { email: data.email },
+          { phoneNumber: data.phoneNumber }
+        ],
+        deleted: false
+      }
+    })
+    if (checkSameUser !== undefined && checkSameUser !== null) {
+      throw new ConflictError('Ya existe una cuenta con esos datos')
+    }
+
+    // Create new user
+    const newUser = await prisma.users.create({
+      data: {
+        ...data,
+        password: bcrypt.hashSync(data.password, 10)
+      }
+    })
+
+    // generate autorized token with id
+    const token = jwt.generateToken({
+      id: newUser.id
+    })
+
+    // generate and send response
+    const response = NextResponse.json(
+      { message: 'Se ha registrado exitosamente' },
+      { status: 201 }
+    )
+    response.cookies.set('token', token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'strict',
+      maxAge: 1000 * 60 * 30,
+      path: '/'
+    })
+    return response
+  } catch (e) {
+    return GenerateErrorResponse(e)
+  }
+}
