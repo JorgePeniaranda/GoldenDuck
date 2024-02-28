b418ea918d990ccae8fa1f6f2eafa4fdcfcf9a13
jorgepeniaranda <peniarandajorge@gmail.com>
2024-02-27 22:38:40 -0300

    feat: create endpoint to verify email with code

---
+++ src/app/api/(un-authorized)/code/[email]/route.ts
@@ -0,0 +1,50 @@
+import ConfirmationCode from '@/services/codeService'
+import { GenerateErrorResponse, ValidationError } from '@/services/errorService'
+import JWT from '@/services/jwtService'
+import validations from '@/services/validationService'
+import { type NextRequest, NextResponse } from 'next/server'
+
+const jwt = new JWT()
+
+export async function GET (
+  req: NextRequest,
+  { params: { email } }: { params: { email: string } }
+): Promise<NextResponse> {
+  const CodeService = new ConfirmationCode()
+
+  try {
+    // validate email
+    await validations.email.parseAsync(email).catch((error) => {
+      throw new ValidationError(error.errors[0].message)
+    })
+
+    // send code to email
+    CodeService.sendCode(email)
+
+    // generate token with code and email
+    const tokenData = {
+      code: CodeService.getCode(),
+      email
+    }
+
+    const token = jwt.generateTempToken(tokenData)
+
+    // generate and send response
+    const response = NextResponse.json(
+      { message: 'Se ha enviado el código de verificación' },
+      { status: 200 }
+    )
+
+    response.cookies.set('token', token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'strict',
+      maxAge: 1000 * 60 * 5,
+      path: '/'
+    })
+
+    return response
+  } catch (e) {
+    return GenerateErrorResponse(e)
+  }
+}
---
+++ src/app/api/(un-authorized)/code/route.ts
@@ -0,0 +1,45 @@
+import ConfirmationCode from '@/services/codeService'
+import { AuthorizationError, GenerateErrorResponse } from '@/services/errorService'
+import JWT from '@/services/jwtService'
+import { type NextRequest, NextResponse } from 'next/server'
+
+const CodeService = new ConfirmationCode()
+const jwt = new JWT()
+
+export async function POST (
+  req: NextRequest
+): Promise<NextResponse> {
+  try {
+    const { code: userCode } = await req.json()
+    const userToken = req.cookies.get('token')?.value
+
+    // validate request
+    if (userCode === undefined) { throw new AuthorizationError('No se ha enviado el código') }
+    if (userToken === undefined) { throw new AuthorizationError('No se ha enviado el token') }
+
+    // verify token and code
+    const { email, code } = jwt.verifyTempToken(userToken)
+    if (!CodeService.checkCode(String(userCode), String(code))) { throw new AuthorizationError('El código es invalido') }
+
+    // generate token with email
+    const token = jwt.generateToken({ email })
+
+    // generate and send response
+    const response = NextResponse.json(
+      { message: 'Se ha enviado el código de verificación' },
+      { status: 200 }
+    )
+
+    response.cookies.set('token', token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'strict',
+      maxAge: 1000 * 60 * 5,
+      path: '/'
+    })
+
+    return response
+  } catch (e) {
+    return GenerateErrorResponse(e)
+  }
+}
