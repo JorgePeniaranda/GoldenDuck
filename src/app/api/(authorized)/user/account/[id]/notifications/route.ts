a4cccda9c44714b9d6892f2917a0d892545ed8ed
jorgepeniaranda <peniarandajorge@gmail.com>
2024-02-29 18:10:29 -0300

    feat: create api structure routes

---
+++ src/app/api/(authorized)/user/account/[id]/notifications/route.ts
@@ -0,0 +1,85 @@
+import { type NextRequest, NextResponse } from 'next/server'
+import { prisma } from '@/libs/prisma'
+import { GenerateErrorResponse, RequestError } from '@/services/errorService'
+import JWT from '@/services/jwtService'
+
+const jwt = new JWT()
+
+export async function GET (request: NextRequest): Promise<NextResponse> {
+  const token = String(
+    request.headers.get('token') ?? request.cookies.get('token')?.value
+  )
+
+  try {
+    const { id } = jwt.verifyToken(token)
+
+    // check if any account exist
+    const data = await prisma.user.findFirst({
+      where: {
+        id,
+        deleted: false
+      },
+      select: {
+        account: {
+          select: {
+            notifications: {
+              where: {
+                read: false
+              },
+              select: {
+                id: true,
+                idAccount: true,
+                message: true,
+                date: true
+              }
+            },
+            messagesFrom: {
+              where: {
+                read: false
+              },
+              select: {
+                id: true,
+                from: true,
+                message: true,
+                date: true,
+                accountFrom: {
+                  select: {
+                    user: {
+                      select: {
+                        name: true,
+                        lastName: true
+                      }
+                    },
+                    imgUrl: true
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    })
+
+    if (data === null) {
+      throw new RequestError('No se ha encontrado la cuenta')
+    }
+
+    const notifications = data.account.reduce(
+      (acc, value) => {
+        return {
+          ...acc,
+          notifications: [...acc.notifications.concat(value.notifications)],
+          messages: [...acc.messages.concat(value.messagesFrom)]
+        }
+      },
+      { notifications: [], messages: [] }
+    )
+
+    notifications.notifications.splice(5)
+    notifications.messages.splice(5)
+
+    return NextResponse.json(notifications, { status: 200 })
+  } catch (error) {
+    return GenerateErrorResponse(error)
+  }
+}
