f37d79d30cb1c5d2032698a35bf9c5171d7db456
jorgepeniaranda <peniarandajorge@gmail.com>
2024-03-01 15:42:10 -0300

    feat: make email, dni and phoneNumber optional parameters

--- src/app/api/(without-token)/verify-user/route.ts
+++ src/app/api/(without-token)/verify-user/route.ts
@@ -2,7 +2,6 @@ import { type NextRequest, NextResponse } from 'next/server'
 import { prisma } from '@/libs/prisma'
 import {
   GenerateErrorResponse,
-  NotFoundError,
   ValidationError
 } from '@/services/errorService'
 import validations from '@/services/validationService'
@@ -10,9 +9,9 @@ import { z } from 'zod'
 import { ReasonPhrases, StatusCodes } from 'http-status-codes'
 
 const Schema = z.object({
-  email: validations.email,
-  dni: validations.dni,
-  phoneNumber: validations.phoneNumber
+  email: validations.email.optional(),
+  dni: validations.dni.optional(),
+  phoneNumber: validations.phoneNumber.optional()
 })
 
 export async function POST (request: NextRequest): Promise<NextResponse> {
@@ -27,19 +26,12 @@ export async function POST (request: NextRequest): Promise<NextResponse> {
     )
 
     // check if any account exist
-    const checkExist = await prisma.user.findFirst({
+    await prisma.user.findFirstOrThrow({
       where: {
         OR: [{ email }, { dni }, { phoneNumber }],
         deleted: false
-      },
-      select: {
-        id: true
       }
     })
-
-    if (checkExist === undefined || checkExist === null) {
-      throw new NotFoundError('No existe una cuenta creada con ese correo')
-    }
 
     return NextResponse.json(ReasonPhrases.OK, { status: StatusCodes.OK })
   } catch (error) {
