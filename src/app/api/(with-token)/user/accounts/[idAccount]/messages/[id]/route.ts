f7dffebc03c6511d671c2089c19f099c8c4c1d82
jorgepeniaranda <peniarandajorge@gmail.com>
2024-03-01 18:33:21 -0300

    feat: add endpoint to get conversation and delete or update messages

---
+++ src/app/api/(with-token)/user/accounts/[idAccount]/messages/[id]/route.ts
@@ -0,0 +1,152 @@
+import { type NextRequest, NextResponse } from 'next/server'
+import { prisma } from '@/libs/prisma'
+import { AuthorizationError, GenerateErrorResponse } from '@/services/errorService'
+import { StatusCodes } from 'http-status-codes'
+import { messages } from '@/const/messages'
+
+export async function GET (request: NextRequest,
+  { params: { id } }: { params: { id: string } }): Promise<NextResponse> {
+  try {
+    const data = await prisma.account.findFirstOrThrow({
+      where: {
+        deleted: false
+      },
+      select: {
+        messagesFrom: {
+          where: {
+            to: Number(id),
+            deleted: false
+          },
+          orderBy: {
+            date: 'asc'
+          },
+          select: {
+            id: true,
+            from: true,
+            message: true,
+            date: true,
+            accountFrom: {
+              select: {
+                user: {
+                  select: {
+                    name: true,
+                    lastName: true
+                  }
+                },
+                imgUrl: true
+              }
+            }
+          }
+        },
+        messagesTo: {
+          where: {
+            from: Number(id),
+            deleted: false
+          },
+          orderBy: {
+            date: 'asc'
+          },
+          select: {
+            id: true,
+            from: true,
+            message: true,
+            date: true,
+            accountFrom: {
+              select: {
+                user: {
+                  select: {
+                    name: true,
+                    lastName: true
+                  }
+                },
+                imgUrl: true
+              }
+            }
+          }
+        }
+      }
+    })
+
+    const conversation = data.messagesFrom.concat(data.messagesTo).reduce<any>((acc, value) => {
+      return [...acc, value]
+    }, [])
+      .sort((a: { date: string }, b: { date: string }) => new Date(a.date).getTime() - new Date(b.date).getTime())
+
+    return NextResponse.json(conversation, { status: StatusCodes.OK })
+  } catch (error) {
+    return GenerateErrorResponse(error)
+  }
+}
+
+export async function POST (request: NextRequest,
+  { params: { idAccount, id } }: { params: { idAccount: string, id: string } }): Promise<NextResponse> {
+  const { message } = await request.json()
+
+  try {
+    // check if account exists
+    await prisma.account.findUniqueOrThrow({
+      where: {
+        id: Number(id)
+      }
+    })
+
+    // create message
+    await prisma.message.create({
+      data: {
+        from: Number(idAccount),
+        to: Number(id),
+        message
+      }
+    })
+
+    // [TO-DO] create notification
+
+    return NextResponse.json(messages.created, { status: StatusCodes.CREATED })
+  } catch (error) {
+    return GenerateErrorResponse(error)
+  }
+}
+
+export async function PUT (request: NextRequest,
+  { params: { idAccount, id } }: { params: { idAccount: string, id: string } }): Promise<NextResponse> {
+  const { message } = await request.json()
+
+  try {
+    await prisma.message.update({
+      where: {
+        id: Number(id),
+        from: Number(idAccount)
+      },
+      data: {
+        message
+      }
+    }).catch(() => {
+      throw new AuthorizationError(messages.noPermissions)
+    })
+
+    return NextResponse.json(messages.updated, { status: StatusCodes.OK })
+  } catch (error) {
+    return GenerateErrorResponse(error)
+  }
+}
+
+export async function DELETE (request: NextRequest,
+  { params: { idAccount, id } }: { params: { idAccount: string, id: string } }): Promise<NextResponse> {
+  try {
+    await prisma.message.update({
+      where: {
+        id: Number(id),
+        from: Number(idAccount)
+      },
+      data: {
+        message: 'messages.deleted'
+      }
+    }).catch(() => {
+      throw new AuthorizationError(messages.noPermissions)
+    })
+
+    return NextResponse.json(messages.deleted, { status: StatusCodes.OK })
+  } catch (error) {
+    return GenerateErrorResponse(error)
+  }
+}
--- src/app/api/(with-token)/user/accounts/[idAccount]/messages/route.ts
+++ src/app/api/(with-token)/user/accounts/[idAccount]/messages/route.ts
@@ -1,68 +1,60 @@
 import { type NextRequest, NextResponse } from 'next/server'
 import { prisma } from '@/libs/prisma'
 import { GenerateErrorResponse } from '@/services/errorService'
-import JWT from '@/services/jwtService'
 import { StatusCodes } from 'http-status-codes'
-import { messages } from '@/const/messages'
 
-const jwt = new JWT()
-
-export async function GET (request: NextRequest): Promise<NextResponse> {
-  const token = String(
-    request.headers.get('token') ?? request.cookies.get('token')?.value
-  )
-
+export async function GET (request: NextRequest,
+  { params: { idAccount } }: { params: { idAccount: string } }): Promise<NextResponse> {
   try {
-    const { id } = jwt.verifyToken(token)
-
-    const data = await prisma.user.findUniqueOrThrow({
+    const data = await prisma.account.findUniqueOrThrow({
       where: {
-        id,
+        id: Number(idAccount),
         deleted: false
       },
       select: {
-        account: {
+        messagesFrom: {
+          where: {
+            read: false,
+            deleted: false
+          },
           select: {
-            messagesFrom: {
-              where: {
-                read: false
-              },
+            id: true,
+            from: true,
+            message: true,
+            date: true,
+            accountFrom: {
               select: {
-                id: true,
-                from: true,
-                message: true,
-                date: true,
-                accountFrom: {
+                imgUrl: true,
+                user: {
                   select: {
-                    user: {
-                      select: {
-                        name: true,
-                        lastName: true
-                      }
-                    },
-                    imgUrl: true
+                    name: true,
+                    lastName: true
                   }
                 }
               }
-            },
-            messagesTo: {
-              where: {
-                read: false
-              },
+            }
+          }
+        },
+        messagesTo: {
+          where: {
+            read: false,
+            deleted: false
+          },
+          orderBy: {
+            date: 'desc'
+          },
+          select: {
+            id: true,
+            from: true,
+            message: true,
+            date: true,
+            accountFrom: {
               select: {
-                id: true,
-                from: true,
-                message: true,
-                date: true,
-                accountFrom: {
+                imgUrl: true,
+                user: {
                   select: {
-                    user: {
-                      select: {
-                        name: true,
-                        lastName: true
-                      }
-                    },
-                    imgUrl: true
+                    name: true,
+                    lastName: true
                   }
                 }
               }
@@ -72,25 +64,7 @@ export async function GET (request: NextRequest): Promise<NextResponse> {
       }
     })
 
-    return NextResponse.json(data.account, { status: StatusCodes.OK })
-  } catch (error) {
-    return GenerateErrorResponse(error)
-  }
-}
-
-export async function POST (request: NextRequest,
-  { params: { idAccount } }: { params: { idAccount: string } }): Promise<NextResponse> {
-  const { message } = await request.json()
-
-  try {
-    await prisma.notification.create({
-      data: {
-        idAccount: Number(idAccount),
-        message
-      }
-    })
-
-    return NextResponse.json(messages.created, { status: StatusCodes.CREATED })
+    return NextResponse.json(data, { status: StatusCodes.OK })
   } catch (error) {
     return GenerateErrorResponse(error)
   }
