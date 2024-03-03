21030d8549d1dff0102533115687ab6f8b6c0257
jorgepeniaranda <peniarandajorge@gmail.com>
2024-03-02 22:48:08 -0300

    api test

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,17 +1,10 @@
 import { NextResponse } from 'next/server'
-import jwt from 'jsonwebtoken'
-import { GenerateErrorResponse } from '@/services/errorService'
 
 const response = (): NextResponse => {
-  try {
-    return NextResponse.json(
-      { date: jwt.verify('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6OSwiaWF0IjoxNzA5MTM0NzE3LCJleHAiOjE3MDkxMzY1MTd9.FuMRP-WSJpQa34AmkAmTEmdRG6b-zz0pzyyxjc2jxes', 'ewq') },
-      { status: 418 }
-    )
-  } catch (e) {
-    console.log(e)
-    return GenerateErrorResponse(e)
-  }
+  return NextResponse.json(
+    { date: new Date().toLocaleString() },
+    { status: 418 }
+  )
 }
 
 export const GET = response
