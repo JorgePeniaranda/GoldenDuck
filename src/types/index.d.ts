87e27771027e594f4ae87a8dcf9706b4f8817b53
jorgepeniaranda <peniarandajorge@gmail.com>
2024-03-03 12:28:49 -0300

    feat: create investment, loan and transaction response interface

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -163,6 +163,21 @@ Omit<User, 'createdAt' | 'updatedAt' | 'role'>
 export type RegisterResponse = Token &
 Omit<User, 'createdAt' | 'updatedAt' | 'role'>
 
+export interface TransactionResponse {
+  newBalance: string | number
+  transaction: Transaction
+}
+
+export interface InvestmentResponse {
+  newBalance: string | number
+  investment: Investment
+}
+
+export interface LoanResponse {
+  newBalance: string | number
+  loan: Loan
+}
+
 export interface ReportError {
   id: number
   name: string
